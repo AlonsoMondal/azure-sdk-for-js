82b768fc0d75d845f83cd1408f6a24b31c42a8fe
Alonso Mondal <alonsomondal@microsoft.com>
2022-01-04 13:26:56 -0600

    resolving comments and re-recording tests

--- sdk/communication/communication-short-codes/src/utils/communicationAccessKeyCredentialPolicy.ts
+++ sdk/communication/communication-short-codes/src/utils/communicationAccessKeyCredentialPolicy.ts
@@ -10,6 +10,7 @@ import {
 import { URL } from "./url";
 import { shaHash, shaHMAC } from "./cryptoUtils";
 export const communicationAccessKeyCredentialPolicyName = "CommunicationAccessKeyCredentialPolicy";
+
 /**
  * Creates an HTTP pipeline policy to authenticate a request using a `KeyCredential`.
  *
@@ -32,7 +33,7 @@ export const createCommunicationAuthPolicy = (
 ): PipelinePolicy => {
   if (isTokenCredential(credential)) {
     return bearerTokenAuthenticationPolicy({
-      scopes: "https://communication.azure.com//.default",
+      scopes: "https://communication.azure.com/.default",
       credential,
     });
   } else {
--- sdk/communication/communication-short-codes/src/utils/url.browser.ts
+++ sdk/communication/communication-short-codes/src/utils/url.browser.ts
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
+/// <reference lib="dom" />
+
 const url = URL;
 const urlSearchParams = URLSearchParams;
 
